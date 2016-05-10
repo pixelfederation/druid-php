5c892822e9592746c283bd174916daab1b80bb7b
Jozef Hrncar <jhrncar@pixelfederation.com>
2016-05-10 09:16:06 +0200

    aggregtor factory edit

--- src/Druid/Query/Component/Factory/AggregatorFactory.php
+++ src/Druid/Query/Component/Factory/AggregatorFactory.php
@@ -28,13 +28,36 @@ class AggregatorFactory
         return new Aggregator\HyperUniqueAggregator($name, $fieldName);
     }
 
-    public function sum($name, $fieldName)
+    public function doubleSum($name, $fieldName)
     {
         return new Aggregator\DoubleSumAggregator($name, $fieldName);
+    }
+
+    public function longSum($name, $fieldName)
+    {
+        return new Aggregator\LongSumAggregator($name, $fieldName);
     }
 
     public function filtered(FilterInterface $filter, AggregatorInterface $aggregator)
     {
         return new Aggregator\FilteredAggregator($filter, $aggregator);
+    }
+
+    public function arithmeticAggregator($type, $name, $fieldName)
+    {
+        switch ($type) {
+            case AggregatorInterface::TYPE_COUNT:
+                return $this->count($name);
+            case AggregatorInterface::TYPE_DOUBLE_SUM:
+                return $this->doubleSum($name, $fieldName);
+            case AggregatorInterface::TYPE_HYPER_UNIQUE:
+                return $this->hyperUnique($name, $fieldName);
+            case AggregatorInterface::TYPE_LONG_SUM:
+                return $this->longSum($name, $fieldName);
+            default:
+                throw new \RuntimeException(
+                    sprintf('Invalid aggregator type %s', $type)
+                );
+        }
     }
 }
